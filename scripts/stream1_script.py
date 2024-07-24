759209b3fbe46a066fc17ef2866210a39d9556a9
andrealtezg <andrealgar03@gmail.com>
2024-07-24 10:46:24 +0200

    Create stream1_script.py

---
+++ scripts/stream1_script.py
@@ -0,0 +1,11 @@
+import time
+
+def stream1():
+    print("Starting Stream1 (EEG)...")
+    while True:
+        # Simular la transmisión de datos EEG
+        print("Streaming EEG data...")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    stream1()
