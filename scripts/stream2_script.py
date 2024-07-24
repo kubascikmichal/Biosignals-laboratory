846078bfc3a288d56434f601de9f78423ef1c639
andrealtezg <andrealgar03@gmail.com>
2024-07-24 10:46:52 +0200

    Create stream2_script.py

---
+++ scripts/stream2_script.py
@@ -0,0 +1,11 @@
+import time
+
+def stream2():
+    print("Starting Stream2 (ECG)...")
+    while True:
+        # Simular la transmisión de datos ECG
+        print("Streaming ECG data...")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    stream2()
